c8e7d9f0050498f6d8b33a88df80b67fb0d808e6
Blackzxy <13258251736@163.com>
2022-05-07 21:30:14 +0200

    update

---
+++ atcode/AtCoder Beginner Contest 246/D.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cstdlib>
+#include<cstring>
+#include<string>
+#include<cmath>
+#include<map>
+#include<set>
+#include<vector>
+#include<queue>
+#include<string>
+#include<bitset>
+#include<ctime>
+#include<deque>
+#include<stack>
+#include<functional>
+#include<sstream>
+typedef long long ll;
+using namespace std;
+typedef unsigned long long int ull;
+#define maxn 200005
+#define ms(x) memset(x,0,sizeof(x))
+#define Inf 0x7fffffff
+#define inf 0x3f3f3f3f
+const int mod = 1e9 + 7;
+#define pi acos(-1.0)
+#define pii pair<int,int>
+#define eps 1e-5
+#define pll pair<ll,ll>
+#define lson 2*x
+#define rson 2*x+1
+
+long long  qupower(int a, int b) {
+	long long  ans = 1;
+	while (b) {
+		if (b & 1)ans = ans * a;
+		b >>= 1;
+		a = a * a;
+	}
+	return ans;
+}
+
+inline int read() {
+	int an = 0, x = 1; char c = getchar();
+	while (c > '9' || c < '0') {
+		if (c == '-') {
+			x = -1; 
+		}
+		c = getchar();
+	}
+	while (c >= '0'&&c <= '9') {
+		an = an * 10 + c - '0'; c = getchar();
+	}
+	return an * x;
+}
+
+ll N;
+ll ans = 1e18+1;
+
+int main(){
+    //ios::sync_with_stdio(false);
+    scanf("%lld",&N);
+    
+    ll a = 1e6;
+    int fg = 0;
+    ll b = 1e6;
+    for(ll i=0;i<=a;i++){
+        while(b>=0 && i*i*i + b*b*b +i*i*b + i*b*b >= N){
+            ans = min(ans,i*i*i + b*b*b +i*i*b +i*b*b);
+            b-=1ll;
+        }
+    }
+    cout<<ans<<endl;
+}
